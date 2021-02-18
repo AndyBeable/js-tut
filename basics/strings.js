6cb05f665a4569bd5a9a2853b8eb531c8ffb002b
AndyBeable <andy.beable@gmail.com>
2021-02-18 23:35:46 +0000

    initial

---
+++ basics/strings.js
@@ -0,0 +1,15 @@
+let name = 'Andy';
+console.log(name);
+
+let firstName = 'Andy';
+let lastName = 'Beable';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// CHALLENGE
+//Store city, country in variables at use concat to print location
+
+let city = 'London,';
+let country = 'United Kingdom';
+let location = city + ' ' + country;
+console.log(location);
