e67ce7ca484be0438b817e1c61969f5986b2dda4
AndyBeable <andy.beable@gmail.com>
2021-02-20 19:16:32 +0000

    temp

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+let petName = 'Arlo';
+petName = 'Spot'; // reassignment of existing variable
+console.log(petName);
+
+// CHALLENGE - Temp conversion
+
+let fahrenheit = 50; // in F is 0c and 273 in Kelvin
+let celsius = ((fahrenheit - 32) * 5) / 9;
+let kelvin = ((fahrenheit + 459.67) * 5) / 9;
+console.log(celsius, 'celsius', kelvin, 'kelvin');
