fc5e4eaca5e66975c23b60e174648b245861a46b
AndyBeable <andy.beable@gmail.com>
2021-02-20 19:02:04 +0000

    revert

---
+++ basics/numbers.js
@@ -0,0 +1,21 @@
+let num = 32 + 4;
+console.log(num);
+
+let x = 11;
+let numb = x + 1 * 2;
+console.log(numb);
+
+let age = 32;
+let dogYears = (age + 1) / 7;
+console.log(dogYears);
+
+// CHALLENGE
+// variable for studentScore
+// variable for maxScore
+// % variable
+// print %
+
+let studentScore = 18;
+let maxScore = 20;
+let percent = (studentScore / maxScore) * 100;
+console.log(percent);
