9ebe2201e6fce840fba1fccd7c1dc5d0dea3c196
AndyBeable <andy.beable@gmail.com>
2021-02-20 19:45:55 +0000

    booleans

---
+++ basics/boolean.js
@@ -0,0 +1,57 @@
+// let temp = 31;
+// let isFreezing = temp === 31; // equality operator
+// let isFreezing2 = temp !== 32; // not equal operator
+// console.log(isFreezing2);
+// console.log(isFreezing);
+
+// let temp = 31;
+// let isFreezing = temp < 32; // less than comparison
+// console.log(isFreezing);
+
+// let temp = 31;
+// let isFreezing = temp > 32; // greater than comparison
+// console.log(isFreezing);
+
+// let temp = 31;
+// let isFreezing = temp >= 32; // greater than or equal to comparison
+// console.log(isFreezing);
+
+// let temp = 31;
+// let isFreezing = temp <= 32; // less than or equal to comparison
+// console.log(isFreezing);
+
+// CHALLENGE
+// create age variable
+// calcuate if child (7 or under)
+// calculate if senior (65+)
+// print both
+
+// let age = 83;
+// let isChild = age <= 7;
+// let isSenior = age >= 65;
+// console.log(isChild, 'Child', isSenior, 'isSenior');
+
+// let temp = 320;
+// // let isFreezing = temp <= 32;
+
+// if (temp <= 32) {
+//   console.log('It is freezing outside!');
+// }
+
+// if (temp >= 110) {
+//   console.log('Lets get to the beach!');
+// }
+
+// CHALLENGE
+// If 7 or under print message about child pricing.
+// if 65 or old print senior message
+
+let age = 3;
+
+if (age <= 7) {
+  console.log('You get a childs rate');
+}
+
+if (age >= 65) {
+  console.log('You pay less for being old');
+}
