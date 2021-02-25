f59543413a6dfadb92cf10b2bcc758dd96948524
AndyBeable <andy.beable@gmail.com>
2021-02-25 18:54:01 +0000

    arrays initial

---
+++ arrays/todo.js
@@ -0,0 +1,12 @@
+const todos = [
+  'Walk the dog',
+  'Go the the gym',
+  'Build portfolio',
+  'Book flights',
+  'Go shopping',
+];
+
+const status = `You have ${todos.length} things to do today`;
+console.log(status);
+console.log(`Todo: ${todos[0]}`);
+console.log(`Todo: ${todos[todos.length - 2]}`);
