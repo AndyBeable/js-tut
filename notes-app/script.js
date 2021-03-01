9baa8701b00286996f52993ebb3d30ac5a01ff31
AndyBeable <andy.beable@gmail.com>
2021-03-01 18:22:58 +0000

    filter

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,4 +1,24 @@
+const notes = [
+  {
+    title: 'My next trip',
+    body: 'I would like to go to Spain',
+  },
+  {
+    title: 'Habits to work on',
+    body: 'Exercise. Eating better',
+  },
+  {
+    title: 'Office Modifications',
+    body: 'Get a new chair',
+  },
+];
+
 const ps = document.querySelectorAll('p');
 ps.forEach(function (p) {
   p.textContent = 'Thing 1';
 });
+
+// Add a new element
+const newParagraph = document.createElement('p');
+newParagraph.textContent = 'You are a cunt';
+document.querySelector('body').appendChild(newParagraph);
--- todo-app/script.js
+++ todo-app/script.js
@@ -1,6 +1,38 @@
-const p = document.querySelectorAll('p');
-p.forEach(function (p) {
-  if (p.textContent.includes('the')) {
-    p.remove();
-  }
-});
+const todos = [
+  {
+    text: 'Walk the dog',
+    completed: true,
+  },
+  {
+    text: 'Go to the gym',
+    completed: true,
+  },
+  {
+    text: 'Build portfolio',
+    completed: true,
+  },
+  {
+    text: 'Book flights',
+    completed: true,
+  },
+  {
+    text: 'Go shopping',
+    completed: false,
+  },
+];
+
+// print summary message. you have 3 todos left for eg
+
+const getThingsTodoDo = function (todos) {
+  return todos.filter(function (todo) {
+    return !todo.completed;
+  });
+};
+
+const incomplete = getThingsTodoDo(todos);
+
+const status = document.createElement('h3');
+status.textContent = `You have ${incomplete.length} things to do today`;
+document.querySelector('body').appendChild(status);
+
+// print paragraph for each todo above. Use text value for the visible content of the p tags.
