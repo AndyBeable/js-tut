bf6e6749828bd0f85fe1d2f592deae9c5bf27ae4
AndyBeable <andy.beable@gmail.com>
2021-03-05 16:14:24 +0000

    hide completed

--- todo-app/script.js
+++ todo-app/script.js
@@ -5,7 +5,7 @@ const todos = [
   },
   {
     text: 'Go to the gym',
-    completed: true,
+    completed: false,
   },
   {
     text: 'Build portfolio',
@@ -23,11 +23,20 @@ const todos = [
 
 const filters = {
   searchText: '',
+  hideCompleted: false,
 };
 
 const renderTodos = function (todos, filters) {
-  const filteredTodos = todos.filter(function (todo) {
+  let filteredTodos = todos.filter(function (todo) {
     return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+  });
+
+  filteredTodos = filteredTodos.filter(function (todo) {
+    if (filters.hideCompleted) {
+      return !todo.completed;
+    } else {
+      return true;
+    }
   });
 
   const incompleteTodos = filteredTodos.filter(function (todo) {
@@ -64,3 +73,10 @@ document.querySelector('#new-todo').addEventListener('submit', function (e) {
   renderTodos(todos, filters);
   e.target.elements.text.value = '';
 });
+
+document
+  .querySelector('#hide-completed')
+  .addEventListener('change', function (e) {
+    filters.hideCompleted = e.target.checked;
+    renderTodos(todos, filters);
+  });
