edd9966dd3e492e25ba3698d1c7c3df17f3fd4b4
AndyBeable <andy.beable@gmail.com>
2021-04-13 18:22:43 +0100

    cb review

--- intro/script.js
+++ intro/script.js
@@ -1,19 +1,49 @@
-function sum(a, b) {
-  return a + b;
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// let total = sum(4, 6);
+// console.log(total);
+
+// function printName(name) {
+//   console.log(name);
+// }
+
+// printName('Iris');
+
+// function sayName(name) {
+//   return 'Hello' + ' ' + name;
+// }
+
+// let greet = sayName('Andy');
+// console.log(greet);
+
+// function printVariable2(variable) {
+//   console.log(variable);
+// }
+
+// function func(x) {
+//   x('hello World');
+// }
+
+// func(printVariable2);
+
+// function sumCallback(a, b, callback) {
+//   callback(a + b);
+// }
+
+// function handleSum(sum) {
+//   console.log(sum);
+// }
+
+// sumCallback(1, 2, handleSum);
+
+function sayHello(name, callback) {
+  callback('Hello ' + name);
 }
 
-let total = sum(4, 6);
-console.log(total);
-
-function printName(name) {
-  console.log(name);
+function printVariable(variable) {
+  console.log(variable);
 }
 
-printName('Iris');
-
-function sayName(name) {
-  return 'Hello' + ' ' + name;
-}
-
-let greet = sayName('Andy');
-console.log(greet);
+sayHello('Andy', printVariable);
