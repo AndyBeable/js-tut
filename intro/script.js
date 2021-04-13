efcbf1fdc2bd8621acfb1252d2745681ecb46153
AndyBeable <andy.beable@gmail.com>
2021-04-13 16:18:50 +0100

    review

---
+++ intro/script.js
@@ -0,0 +1,17 @@
+function sayName() {
+  console.log('Andy');
+}
+
+sayName();
+
+function sum(a, b) {
+  console.log(a + b);
+}
+
+sum(4, 6);
+
+function printName(name) {
+  console.log(name);
+}
+
+printName('Iris');
