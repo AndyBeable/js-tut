4d5c7fc5356b9db13ed194093a02f1b8c4dd4a10
AndyBeable <andy.beable@gmail.com>
2021-04-26 16:33:14 +0100

    arrw func diff

--- dom/script.js
+++ dom/script.js
@@ -36,17 +36,30 @@
 
 // button.removeEventListener('click', greeting);
 
-const button = document.querySelector('[data-btn]');
-const input = document.querySelector('[data-input-text]');
-const form = document.querySelector('[data-form]');
-const anchor = document.querySelector('[anchor-btn]');
+// const button = document.querySelector('[data-btn]');
+// const input = document.querySelector('[data-input-text]');
+// const form = document.querySelector('[data-form]');
+// const anchor = document.querySelector('[anchor-btn]');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  console.log('submitted form');
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   console.log('submitted form');
+// });
+
+// anchor.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   console.log('Clicked');
+// });
+
+// ARROW FUNCS CONTINUED
+const button = document.querySelector('button');
+
+button.addEventListener('click', (e) => {
+  console.log('Arrow this');
+  console.log(this);
 });
 
-anchor.addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log('Clicked');
+button.addEventListener('click', function (e) {
+  console.log('function this');
+  console.log(this);
 });
