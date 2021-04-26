821acd6f504189534cc2f5479d7fc7d6b7254fcc
AndyBeable <andy.beable@gmail.com>
2021-04-26 11:36:09 +0100

    query selectors rev

--- dom/script.js
+++ dom/script.js
@@ -1,10 +1,25 @@
-const divWithId = document.getElementById('div-id');
+// ID/CLASS SELECTORS
 
-divWithId.style.color = 'Red';
+// const divWithId = document.getElementById('div-id');
 
-const divWithClass = document.getElementsByClassName('div-class');
-console.log(divWithClass);
+// divWithId.style.color = 'Red';
 
-const divWithClassArray = Array.from(divWithClass);
+// const divWithClass = document.getElementsByClassName('div-class');
+// console.log(divWithClass);
 
-divWithClassArray.forEach((div) => (div.style.color = 'green'));
+// const divWithClassArray = Array.from(divWithClass);
+
+// divWithClassArray.forEach((div) => (div.style.color = 'green'));
+
+// QUERY SELECTORS
+
+const dataAttributeEl = document.querySelector('[data-test]');
+
+const divsWithClasses = document.querySelectorAll('.div-class');
+
+const input = document.querySelector('input');
+console.log(input);
+
+divsWithClasses.forEach((div) => (div.style.color = 'green'));
+
+console.log(divsWithClasses);
