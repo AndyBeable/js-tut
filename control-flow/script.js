0d2d052db12e36ab044539ab5f6d203b2c5738fa
AndyBeable <andy.beable@gmail.com>
2021-04-27 16:58:30 +0100

    ternary rev

--- control-flow/script.js
+++ control-flow/script.js
@@ -1,39 +1,63 @@
-const userIsLoggedIn = false;
+// IF / ELSE IF
 
-if (userIsLoggedIn) {
-  console.log('User logged in');
-} else {
-  console.log('Please login');
-}
+// const userIsLoggedIn = false;
 
-const totalPrice = 15;
+// if (userIsLoggedIn) {
+//   console.log('User logged in');
+// } else {
+//   console.log('Please login');
+// }
 
-if (totalPrice == null) {
-  console.log('there is no total price');
-} else if (totalPrice < 10) {
-  console.log('This is cheap');
-} else if (totalPrice < 50) {
-  console.log('This is good value');
-} else {
-  console.log('this is expensive');
-}
+// const totalPrice = 15;
 
-const sports = [
-  'Football',
-  'Golf',
-  'Tennis',
-  'Motorsports',
-  'Hockey',
-  'Boxing',
-  'F1',
-];
+// if (totalPrice == null) {
+//   console.log('there is no total price');
+// } else if (totalPrice < 10) {
+//   console.log('This is cheap');
+// } else if (totalPrice < 50) {
+//   console.log('This is good value');
+// } else {
+//   console.log('this is expensive');
+// }
 
-if (sports.length === 0) {
-  console.log('empty');
-} else if (sports.length < 5) {
-  console.log('small');
-} else if (sports.length < 10) {
-  console.log('medium');
-} else {
-  console.log('Large');
-}
+// const sports = [
+//   'Football',
+//   'Golf',
+//   'Tennis',
+//   'Motorsports',
+//   'Hockey',
+//   'Boxing',
+//   'F1',
+// ];
+
+// if (sports.length === 0) {
+//   console.log('empty');
+// } else if (sports.length < 5) {
+//   console.log('small');
+// } else if (sports.length < 10) {
+//   console.log('medium');
+// } else {
+//   console.log('Large');
+// }
+
+// SINGLE LINE IF STATEMENTS
+const isUserLoggedIn = false;
+// if (isUserLoggedIn) console.log('User is logged in');
+
+// TERNARY OPERATORS
+
+// isUserLoggedIn
+//   ? console.log('user is logged in')
+//   : console.log('please log in');
+
+// let welcomeMessage = '';
+
+// if (isUserLoggedIn) {
+//   welcomeMessage = 'Welcome';
+// } else {
+//   welcomeMessage = 'Please log in';
+// }
+
+const welcomeMessage = isUserLoggedIn ? 'Welcome' : 'Please log in';
+
+console.log(welcomeMessage);
