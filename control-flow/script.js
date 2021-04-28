6dff2dd8dd8cb3c4d6ae4cbde62ba76e22947cfa
AndyBeable <andy.beable@gmail.com>
2021-04-28 11:49:26 +0100

    switch rev

--- control-flow/script.js
+++ control-flow/script.js
@@ -41,7 +41,7 @@
 // }
 
 // SINGLE LINE IF STATEMENTS
-const isUserLoggedIn = false;
+// const isUserLoggedIn = false;
 // if (isUserLoggedIn) console.log('User is logged in');
 
 // TERNARY OPERATORS
@@ -58,6 +58,58 @@ const isUserLoggedIn = false;
 //   welcomeMessage = 'Please log in';
 // }
 
-const welcomeMessage = isUserLoggedIn ? 'Welcome' : 'Please log in';
+// const welcomeMessage = isUserLoggedIn ? 'Welcome' : 'Please log in';
 
-console.log(welcomeMessage);
+// console.log(welcomeMessage);
+
+// SWITCH STATEMENTS
+
+const favouriteAnimal = 'cat';
+
+// if (favouriteAnimal === 'cat') {
+//   console.log('cats are cool');
+// } else if (favouriteAnimal === 'dog') {
+//   console.log('Dogs are cool too');
+// } else if (favouriteAnimal === 'shark') {
+//   console.log('Strange choice');
+// } else {
+//   console.log('Dont know that animal');
+// }
+
+// refactored in switch statement
+
+// switch (favouriteAnimal) {
+//   case 'kitten':
+//   case 'cat':
+//     console.log('Cats are cool');
+//     break;
+//   case 'dog':
+//     console.log('Dogs are also cool');
+//     break;
+//   case 'shark':
+//     console.log('Strange choice');
+//     break;
+//   default:
+//     console.log('Dont know that animal');
+// }
+
+const number = 1;
+
+switch (number) {
+  case 0:
+    console.log('It is zero');
+    break;
+  case 1:
+  case 2:
+    console.log('It is small');
+    break;
+  case 3:
+  case 4:
+    console.log('It is medium');
+    break;
+  case 5:
+    console.log('It is large');
+    break;
+  default:
+    console.log('Try again');
+}
