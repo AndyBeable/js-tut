402f78ad780396952de168219337191032a36876
AndyBeable <andy.beable@gmail.com>
2021-06-10 15:29:40 +0100

    callbacks fnl

--- callbacks/script.js
+++ callbacks/script.js
@@ -1,3 +1,23 @@
 setTimeout(() => {
-  console.log('Hi');
-}, 5000);
+  console.log('inside');
+  setTimeout(() => {
+    console.log('inside 2');
+    setTimeout(() => {
+      console.log('inside 3');
+    }, 100);
+  }, 100);
+}, 1000);
+
+const button = document.querySelector('button');
+
+addClickEventListener(button, () => {
+  console.log('Clicked');
+});
+
+console.log('outside');
+
+// Callbacks - func that gets passed to another func in order to executed at a later time based on some condition (time, event etc)
+
+function addClickEventListener(element, callback) {
+  element.addEventListener('click', callback);
+}
