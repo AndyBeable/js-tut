5697f829f637828619cb6a71571a9184dad76827
AndyBeable <andy.beable@gmail.com>
2021-06-11 15:32:52 +0100

    async refac

--- async/script.js
+++ async/script.js
@@ -1,25 +1,38 @@
 function setTimeoutPromise(delay) {
   return new Promise((resolve, reject) => {
     setTimeout(resolve, delay);
-  });
+    reject('Error');
+    // resolve(`You waited ${delay} milliseconds`);
+  }, delay);
 }
 
 // setTimeoutPromise(250)
-//   .then(() => {
+//   .then((message) => {
+//     console.log(message);
 //     console.log('1');
 //     return setTimeoutPromise(350);
 //   })
-//   .then(() => {
+//   .then((message) => {
+//     console.log(message);
 //     console.log('2');
+//   })
+//   .catch((error) => {
+//     console.error(error);
 //   });
 
 // refactoring using async
 
 async function doStuff() {
-  await setTimeoutPromise(250);
-  console.log('1');
-  await setTimeoutPromise(250);
-  console.log('2');
+  try {
+    const message = await setTimeoutPromise(250);
+    console.log(message);
+    console.log('1');
+    const message2 = await setTimeoutPromise(250);
+    console.log(message2);
+    console.log('2');
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 doStuff();
